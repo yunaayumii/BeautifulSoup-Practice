0bec12dc16655e28710e3869f833337fe8a88740
Yuan Miguel S. Rubio <yuanmiguelrubio@gmail.com>
2024-10-13 15:54:10 +0800

    Update news_scraper.py
    this is the completed version of the code

--- News_Scraper/news_scraper.py
+++ News_Scraper/news_scraper.py
@@ -6,5 +6,14 @@ url = 'https://news.ycombinator.com/'
 html = requests.get(url)
 
 #parse the html content 
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(html.content, "html.parser")
 
+#get all the news headers
+incomplete_headers = soup.find_all('span', class_="titleline")
+
+#isolate the title and print out with a number
+for i, incomplete_header in enumerate(incomplete_headers):
+    link_header = incomplete_header.find('a')
+    title_of_header = link_header.text
+    number = i + 1
+    print(f"{number}. {title_of_header} ")
